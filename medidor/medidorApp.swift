9560bd1b4b85aab62a28af5e7bbcc32a03216362
daniel melenge <danielmelenge@gmail.com>
2023-04-02 14:20:47 -0500

    Initial Commit

---
+++ medidor/medidorApp.swift
@@ -0,0 +1,17 @@
+//
+//  medidorApp.swift
+//  medidor
+//
+//  Created by daniel melenge rojas on 2/04/23.
+//
+
+import SwiftUI
+
+@main
+struct medidorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
